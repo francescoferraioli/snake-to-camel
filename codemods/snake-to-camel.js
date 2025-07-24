2e2374a4cd34f02d857000ff3c35987418730303
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2025-07-24 21:25:30 +1000

    Be more strict about snake_case

--- codemods/snake-to-camel.js
+++ codemods/snake-to-camel.js
@@ -1,7 +1,8 @@
 const { camelCase } = require('lodash');
 
 function isSnakeCase(name) {
-  return /_/.test(name);
+  // Check for snake_case pattern: all lowercase with underscores
+  return /^[a-z]+(_[a-z0-9]+)*$/.test(name);
 }
 
 function toCamelCase(name) {
