865b41eb7c936655e3afbaa432d80694cd5325e1
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2025-07-26 08:56:58 +1000

    Allow to supply log file

--- scripts/csv.sh
+++ scripts/csv.sh
@@ -1,1 +1,5 @@
-cat ignore/snake-to-camel.log | grep 'SNAKE_TO_CAMEL_CSV:' | sed 's/^SNAKE_TO_CAMEL_CSV://'
+#!/bin/bash
+
+LOG_FILE=${1:-ignore/snake-to-camel.log}
+
+cat $LOG_FILE | grep 'SNAKE_TO_CAMEL_CSV:' | sed 's/^SNAKE_TO_CAMEL_CSV://'
